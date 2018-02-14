daf673e4d03029c8b69eab869c3077f4758598eb
Alex Shapiro <acjshapiro@gmail.com>
2018-02-13 18:20:21 -0700

    comment main.js

--- js/main.js
+++ js/main.js
@@ -2,28 +2,36 @@ var uChoice = null      //Sets variables that can be used throughout.
 var cChoice = null      //These are set in the functions below.
 
 
-function computerChoice(){
-  var arrayRps = ["rock", "paper", "scissors"]
-  var compChoice = arrayRps[Math.floor(Math.random()* 3)]
-  cChoice =  compChoice
-  console.log(compChoice)
+function computerChoice(){    //function to get random computer choice R/P/S
+  var arrayRps = ["rock", "paper", "scissors"]   //init array with R/P/S
+  var compChoice = arrayRps[Math.floor(Math.random()* 3)]  //creat variable to get/store random R/P/S instead of arrayRps[0,1,2]
+  cChoice =  compChoice       //assign local variable compChoice to document variable choice
+  console.log(compChoice)     //print to console to for testing. ensure random generation
 }
 
-function startGame(e) {
-  var choice = e.target.id
-  uChoice = choice
-  console.log(choice)
-  computerChoice()
-  console.log(compare())
+function startGame(e) {             //start game function(element)
+  var choice = e.target.id     //assign the user choice from target.id to local variable choice
+  uChoice = choice             //assign choice to uChoice ^ from top. reassigns from null
+  console.log(choice)          // prints choice to console for testing
+  computerChoice()            //runs function computerChoice()
+  console.log(compare())      //will print the results of my compare function below to console for testing
 }
 
 
-// }
+// outcomes to add to compare() function
+          // TIES: uChoice == cChoice ( Rock rock, paper paper, scissors scissors)
+          // Rock paper  : User loses
+          // Rock Scissors : user wins
+          // Paper Rock  : user wins
+          // Paper Scissors : user loses
+          // Scissors Paper : user wins
+          // scissors Rock  : user loses
+
 
 function compare(){
   if (uChoice == cChoice){
     return "tie"
-  }else if( ....   ) {
+  }else if(uChoice == "rock" && cChoice == "paper" ) {
 
 
   }else if (......){

983a9a5e19ccce38e18714fd64a046f1a7f3d68f
Alex Shapiro <acjshapiro@gmail.com>
2018-02-13 17:18:29 -0700

    defining funtions and variables

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
+var uChoice = null      //Sets variables that can be used throughout.
+var cChoice = null      //These are set in the functions below.
+
+
+function computerChoice(){
+  var arrayRps = ["rock", "paper", "scissors"]
+  var compChoice = arrayRps[Math.floor(Math.random()* 3)]
+  cChoice =  compChoice
+  console.log(compChoice)
+}
+
 function startGame(e) {
   var choice = e.target.id
+  uChoice = choice
   console.log(choice)
+  computerChoice()
+  console.log(compare())
 }
+
+
+// }
+
+function compare(){
+  if (uChoice == cChoice){
+    return "tie"
+  }else if( ....   ) {
+
+
+  }else if (......){
+
+  }
+
+
+}
+
+
 
 var choices = document.getElementsByClassName('choice')
 for (var i in choices) {
